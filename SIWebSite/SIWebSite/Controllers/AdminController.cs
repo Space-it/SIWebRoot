f7a0f25be544c35a8be2e1a9c0baefd2713827cf
Maxim <maxim.tis96@gmail.com>
2016-05-10 17:19:50 +0300

    Working on Admin panel

---
+++ SIWebSite/SIWebSite/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SIWebSite.WebCore.Models;
+
+namespace SIWebSite.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Person model)
+        {
+            ViewBag.Message = "Welcome " + model.Login + " !";
+            return View("Index");
+        }
+    }
+}
---
+++ SIWebSite/SIWebSite/WebCore/Models/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIWebSite.WebCore.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string LastName { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public int Id { get; set; }
+    }
+}
