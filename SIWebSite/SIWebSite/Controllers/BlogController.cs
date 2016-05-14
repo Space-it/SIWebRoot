3f5a8644d47493534f0411c91ab09edd47325fc6
Maxim <maxim.tis96@gmail.com>
2016-05-14 04:40:22 +0300

    Start working on Blog, add new design

---
+++ SIWebSite/SIWebSite/Controllers/BlogController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SIWebSite.WebCore.DataBase;
+using SIWebSite.WebCore.Models;
+
+namespace SIWebSite.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        public ActionResult Index()
+        {
+            return View(LocalDB.Entryes);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Entry model)
+        {
+            LocalDB.Entryes.Add(model);
+            return Redirect("Index");
+        }
+    }
+}
---
+++ SIWebSite/SIWebSite/WebCore/Models/Entry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIWebSite.WebCore.Models
+{
+    public class Entry
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int AuthorId { get; set; }
+        public DateTime CreateDate { get; set; }
+        public  string Image { get; set; }
+    }
+}
